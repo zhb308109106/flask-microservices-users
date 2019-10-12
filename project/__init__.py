9aa30c00304eef861dcaaab0412cd19d3d88a8e1
zhb <308109106@qq.com>
2019-10-12 08:35:56 +0000

    deploy ok

--- project/__init__.py
+++ project/__init__.py
@@ -1,14 +1,25 @@
 # project/__init__.py
 from flask import Flask, jsonify
-
+import os
+from flask_sqlalchemy import SQLAlchemy
+#import sys
 # 初始化app
 app = Flask(__name__)
 # 环境配置
-app.config.from_object('project.config.DevelopmentConfig')
+#app.config.from_object('project.config.DevelopmentConfig')
 
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+# 初始化数据库
+db = SQLAlchemy()
+def create_app():
+    # 初始化应用
+    app = Flask(__name__)
+    # 环境配置
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+    # 安装扩展
+    db.init_app(app)
+    # 注册blueprint
+    from project.api.views import users_blueprint
+    app.register_blueprint(users_blueprint)
+    return app
+
---
+++ project/api/views.py
@@ -0,0 +1,105 @@
+# project/api/views.py
+from flask import Blueprint, jsonify, request, render_template
+from project import db
+from project.api.models import User
+from sqlalchemy import exc
+
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
+
+@users_blueprint.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+
+    users = User.query.order_by(User.created_at.desc()).all()
+    return render_template('index.html',users=users)
+
+@users_blueprint.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
+
+@users_blueprint.route('/users', methods=['POST'])
+def add_user():
+    # 获取POST的数据
+    post_data = request.get_json()
+    if not post_data:
+        response_data = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_data), 400
+    email = post_data.get('email')
+    username = post_data.get('username')
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            # 证明数据库中不存在该email的用户，可以添加
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+            response_data = {
+                'status': 'success',
+                'message': '%s was added!' % email
+            }
+            return jsonify(response_data), 201
+        # 证明该email已经存在
+        response_data = {
+            'status': 'fail',
+            'message': 'Sorry. That email already exists.'
+        }
+        return jsonify(response_data), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()  # 出现异常了，回滚
+        response_data = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_data), 400
+@users_blueprint.route('/users', methods=['GET'])
+def get_users():
+   users = User.query.all()
+   userlist = []
+   if users:
+      for user in users:
+        userlist.append({'username': user.username,'email': user.email,'created_at': user.created_at,"id":user.id})
+   response_object = {
+                'status': 'success',
+                'data': {
+                     "users": userlist
+                }
+            }
+   code = 200
+   return jsonify(response_object), code
+@users_blueprint.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    """获取某用户的详细信息"""
+    response_object = {
+        'status': 'fail',
+        'message': 'User does not exist'
+    }
+    code = 404
+    try:
+        user = User.query.filter_by(id=int(user_id)).first()
+        if user:
+            response_object = {
+                'status': 'success',
+                'data': {
+                    'username': user.username,
+                    'email': user.email,
+                    'created_at': user.created_at
+                }
+            }
+            code = 200
+    except ValueError:
+        response_object = {
+            'status': 'fail',
+            'message': 'Param id error'
+        }
+        code = 400
+    finally:
+        return jsonify(response_object), code
